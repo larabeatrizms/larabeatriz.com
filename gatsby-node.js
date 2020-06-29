6c82b9ef63cca70427197062c209a4b9147c33d5
Lara Beatriz <laraamori.machado@gmail.com>
2020-06-29 01:40:14 -0300

    fix build issue

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,45 +1,45 @@
-const path = require('path');
+// const path = require('path');
 
-// exports.onCreateWebpackConfig = ({ actions }) => {
-//   actions.setWebpackConfig({
-//     resolve: {
-//       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
-//     },
+// // exports.onCreateWebpackConfig = ({ actions }) => {
+// //   actions.setWebpackConfig({
+// //     resolve: {
+// //       modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+// //     },
+// //   });
+// // };
+
+// exports.createPages = ({ graphql, actions }) => {
+//   const { createPage } = actions;
+//   const post = path.resolve('src/templates/post.js');
+
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             html
+//             frontmatter {
+//               title
+//               path
+//               date(formatString: "DD/MM")
+//             }
+//           }
+//         }
+//       }
+//     }
+//   `).then(result => {
+//     const posts = result.data.allMarkdownRemark.edges;
+
+//     posts.forEach(({ node }) => {
+//       const { frontmatter } = node;
+
+//       createPage({
+//         path: frontmatter.path,
+//         component: post,
+//         context: {
+//           path: frontmatter.path,
+//         },
+//       });
+//     });
 //   });
 // };
-
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
-  const post = path.resolve('src/templates/post.js');
-
-  return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            frontmatter {
-              title
-              path
-              date(formatString: "DD/MM")
-            }
-          }
-        }
-      }
-    }
-  `).then(result => {
-    const posts = result.data.allMarkdownRemark.edges;
-
-    posts.forEach(({ node }) => {
-      const { frontmatter } = node;
-
-      createPage({
-        path: frontmatter.path,
-        component: post,
-        context: {
-          path: frontmatter.path,
-        },
-      });
-    });
-  });
-};
